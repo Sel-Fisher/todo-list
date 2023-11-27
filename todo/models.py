a675c0223b984c7a8136b61977b7c1611586710a
Denis Komandyr <sabsdaid@gmail.com>
2023-11-27 11:17:38 +0200

    Correct quotes in manage.py, todo and todo_list. Add first models and add todo to installed application in settings

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Tags(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    date_of_complete = models.DateTimeField(null=True, blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tags, related_name="tasks", blank=True)
+
+    class Meta:
+        ordering = ["-date_of_creation"]
+
+    def __str__(self):
+        return self.name
+
+    def check_deadline(self):
+        if self.date_of_complete:
+            return self.date_of_complete <= self.deadline
+        return False
