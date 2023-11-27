cb3ebf3fb84e06bac9e9831a752b74dc141bdb41
Denis Komandyr <sabsdaid@gmail.com>
2023-11-27 11:57:49 +0200

    Create task views, base templates html. Add static file

---
+++ todo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="home"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+]
+
+app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:home")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+
+    def get_success_url(self):
+        return reverse_lazy("todo:task-detail", args=[self.object.pk])
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:home")
